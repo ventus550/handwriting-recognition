2c259b4b45c1abefd3e30ac7dab8c873579ad850
Ventus <ventuss550@gmail.com>
2023-04-11 17:57:14 +0200

    refactor: utils and model

--- utils.py
+++ utils.py
@@ -2,7 +2,8 @@ import tensorflow as tf
 from tensorflow import keras
 from pathlib import Path
 
-def save(model, path: Path, metadata = {}):
+def save(model, path: Path | str, metadata = {}):
+	path = Path(path)
 	weights = model.get_weights()
 	config = model.get_config()
 	config["name"] = {"name": config["name"], **metadata}
@@ -11,7 +12,8 @@ def save(model, path: Path, metadata = {}):
 	model.save(path)
 	return model
 
-def load(path: Path):
+def load(path: Path | str):
+	path = Path(path)
 	model = keras.models.load_model(path)
 	model.meta = model.get_config()["name"]
 	model._name = path.name
