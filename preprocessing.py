05a00aa3f14f3a8d7735c3060b59dd437b727b6c
Ventus <ventuss550@gmail.com>
2023-04-29 13:00:32 +0200

    feat: integate new oracle into the app

--- preprocessing.py
+++ preprocessing.py
@@ -11,8 +11,8 @@ from contextlib import contextmanager
 import cv2
 
 
-image_size = (128, 32)  # WxH
-DESTDIR = Path(os.getcwd()) / "preprocessing"
+image_size = (32, 128)  # WxH
+DESTDIR = Path() / "preprocessing"
 Path.mkdir(DESTDIR, parents=True, exist_ok=True)
 
 
@@ -27,13 +27,10 @@ def window(fn):
 
 def store_transformation(transformation):
     def inner(x):
-        assert x.shape[:2][::-1] == image_size, x.shape[:2][::-1]
         with window(transformation.__name__):
             tr = transformation(x)
-            keras.preprocessing.image.save_img(
-                str(DESTDIR / transformation.__name__) + ".png",
-                tf.reshape(tr, (*image_size[::-1], 1)),
-            )
+            dest = str(DESTDIR / transformation.__name__) + ".png"
+            keras.preprocessing.image.save_img(dest, tr)
         return tr
 
     return inner
@@ -61,18 +58,12 @@ def resize(image, size=image_size):
     return image.resize(size)
 
 
-# @image_conversion
+@store_transformation
 def pad_resize(image, size=image_size):
-    image = tf.image.resize(image, size=size[::-1], preserve_aspect_ratio=True)
-    padding = np.array(size[::-1]) - np.array(image.shape)[:2]
-    W, H = padding // 2
-    w, h = padding % 2
-
-    return tf.pad(image, paddings=[[W, W + w], [H, H + h], [0, 0]], constant_values=255)
+    return tf.image.resize_with_pad(255 - image, *size, method=tf.image.ResizeMethod.LANCZOS3)
 
 
 @store_transformation
-# @image_conversion
 def grayscale(image):
     return tf.image.rgb_to_grayscale(image)
 
@@ -97,7 +88,7 @@ def dilate(image):
 
 @store_transformation
 def normalize(image):
-    return tf.cast(image, tf.float32) / 255.0
+    return np.where(tf.cast(image, tf.float32) / 255.0 > 0, 1, 0)
 
 
 @store_transformation
@@ -137,6 +128,8 @@ def preprocess(image: Image):
         input,
         pad_resize,
         grayscale,
-        invertnorm,
-        reshape,
+        normalize,
+        # skeletonize,
+        # invertnorm,
+        # reshape,
     )
